0332547f741a4d69110df7500a96c7e5cc4cc410
bbag <bill.baggerman@gmail.com>
2026-02-07 00:51:08 -0500

    feat: added local fonts; cloudflare adapter; sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,81 +4,88 @@ import mdx from '@astrojs/mdx'
 import icon from 'astro-icon'
 import tailwindcss from '@tailwindcss/vite'
 
+import sitemap from '@astrojs/sitemap'
+
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.fluid-plumbing-llc',
-	base: '/',
-	output: 'static',
+  site: 'https://fluidplumbingllc.com',
+  base: '/',
+  output: 'static',
 
-	integrations: [
-		icon({
-			include: {
-				// Include Bootstrap icons used in the project
-				bi: [
-					'arrow-left',
-					'arrow-right',
-					'arrow-right',
-					'arrow-up',
-					'award',
-					'award-fill',
-					'bar-chart-fill',
-					'calendar-check',
-					'check-circle-fill',
-					'chevron-down',
-					'chevron-left',
-					'chevron-right',
-					'clock-history',
-					'cone-striped',
-					'currency-dollar',
-					'envelope',
-					'envelope-fill',
-					'eye',
-					'flag',
-					'flower1',
-					'folder-fill',
-					'gear-fill',
-					'gem',
-					'geo-alt',
-					'geo-alt-fill',
-					'graph-up',
-					'graph-up-arrow',
-					'heart-fill',
-					'house-door',
-					'house-door-fill',
-					'houses',
-					'info-circle',
-					'journal-check',
-					'list',
-					'map',
-					'people',
-					'person',
-					'person-check',
-					'search',
-					'send',
-					'send-fill',
-					'shield',
-					'shield-check',
-					'star-fill',
-					'star-half',
-					'tag',
-					'telephone',
-					'telephone-fill',
-					'truck',
-					'truck-front',
-					'wrench',
-					'x-lg',
-				]
-			}
-		}),
-		mdx()
+  integrations: [
+      icon({
+          include: {
+              // Include Bootstrap icons used in the project
+              bi: [
+                  'arrow-left',
+                  'arrow-right',
+                  'arrow-right',
+                  'arrow-up',
+                  'award',
+                  'award-fill',
+                  'bar-chart-fill',
+                  'calendar-check',
+                  'check-circle-fill',
+                  'chevron-down',
+                  'chevron-left',
+                  'chevron-right',
+                  'clock-history',
+                  'cone-striped',
+                  'currency-dollar',
+                  'envelope',
+                  'envelope-fill',
+                  'eye',
+                  'flag',
+                  'flower1',
+                  'folder-fill',
+                  'gear-fill',
+                  'gem',
+                  'geo-alt',
+                  'geo-alt-fill',
+                  'graph-up',
+                  'graph-up-arrow',
+                  'heart-fill',
+                  'house-door',
+                  'house-door-fill',
+                  'houses',
+                  'info-circle',
+                  'journal-check',
+                  'list',
+                  'map',
+                  'people',
+                  'person',
+                  'person-check',
+                  'search',
+                  'send',
+                  'send-fill',
+                  'shield',
+                  'shield-check',
+                  'star-fill',
+                  'star-half',
+                  'tag',
+                  'telephone',
+                  'telephone-fill',
+                  'truck',
+                  'truck-front',
+                  'wrench',
+                  'x-lg'
+              ]
+          }
+      }),
+      mdx(),
+      sitemap()
 	],
 
-	vite: {
-		resolve: {
-			alias: {
-				'@img': '/src/img'
-			}
-		},
-		plugins: [tailwindcss()]
-	}
+  vite: {
+      resolve: {
+          alias: {
+              '@img': '/src/img'
+          }
+      },
+      plugins: [tailwindcss()]
+	},
+
+  adapter: cloudflare()
 })
